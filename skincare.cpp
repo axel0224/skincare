7e7c5d31ce3e0ac5629ee1f42b26e0344f4532be
axel0224 <alm100620@gmail.com>
2025-01-02 19:49:09 -0600

    First commit

---
+++ skincare.cpp
@@ -0,0 +1,19 @@
+/*
+    Name: Axel Lopez
+    Date: 01-02-25
+    Description: Keep up with my skincare products and information about them as well as my routine.
+    File: skincare.cpp
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout << "Skincare app" << endl;
+
+    cout << "What would you like to do?" << endl;
+    cout << ""
+    return 0;
+}
